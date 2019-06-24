7b1912fab1f077c4eb08cd2380d9a49bf38bc909
jaygroover1 <jaygroover@gmail.com>
2019-06-23 22:53:13 -0400

    server

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var method = require("method-override");
+var body = require("body-parser");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+var cheerio = require("cheerio");
+var request = require("request");
+
+
+var app = express();
+var port = process.env.PORT || 3000;
