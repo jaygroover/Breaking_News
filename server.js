dc232c6920bdc95d262e070ddc1d564ded9838d1
jaygroover1 <jaygroover@gmail.com>
2019-06-23 23:07:59 -0400

    working server

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 var express = require("express");
-var method = require("method-override");
+
 var body = require("body-parser");
 var exphbs = require("express-handlebars");
 var mongoose = require("mongoose");
 var logger = require("morgan");
 var cheerio = require("cheerio");
-var request = require("request");
 
 
+
+
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+
+
+var PORT = 3000;
+
+// Initialize Express
 var app = express();
-var port = process.env.PORT || 3000;
+
+
+
+
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
